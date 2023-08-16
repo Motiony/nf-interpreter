084e4f3a0cac263212df7d336b558e38d8d80fb7
MrRadian <62394971+MrRadian@users.noreply.github.com>
2023-08-16 18:34:31 +0200

    init settings

---
+++ InteropAssemblies/NFLvgl/NFLvgl_NFLvgl_Lvgl.cpp
@@ -0,0 +1,33 @@
+//-----------------------------------------------------------------------------
+//
+//                   ** WARNING! ** 
+//    This file was generated automatically by a tool.
+//    Re-running the tool will overwrite this file.
+//    You should copy this file to a custom location
+//    before adding any customization in the copy to
+//    prevent loss of your changes when the tool is
+//    re-run.
+//
+//-----------------------------------------------------------------------------
+
+#include "NFLvgl.h"
+#include "NFLvgl_NFLvgl_Lvgl.h"
+
+using namespace NFLvgl::NFLvgl;
+
+
+void Lvgl::NativeInitDisplay(  HRESULT &hr )
+{
+
+    (void)hr;
+
+
+    ////////////////////////////////
+    // implementation starts here //
+
+
+    // implementation ends here   //
+    ////////////////////////////////
+
+
+}
---
+++ InteropAssemblies/NFLvgl/NFLvgl_NFLvgl_Lvgl.h
@@ -0,0 +1,31 @@
+//-----------------------------------------------------------------------------
+//
+//                   ** WARNING! ** 
+//    This file was generated automatically by a tool.
+//    Re-running the tool will overwrite this file.
+//    You should copy this file to a custom location
+//    before adding any customization in the copy to
+//    prevent loss of your changes when the tool is
+//    re-run.
+//
+//-----------------------------------------------------------------------------
+
+#ifndef NFLVGL_NFLVGL_NFLVGL_LVGL_H
+#define NFLVGL_NFLVGL_NFLVGL_LVGL_H
+
+namespace NFLvgl
+{
+    namespace NFLvgl
+    {
+        struct Lvgl
+        {
+            // Helper Functions to access fields of managed object
+            // Declaration of stubs. These functions are implemented by Interop code developers
+
+            static void NativeInitDisplay(  HRESULT &hr );
+
+        };
+    }
+}
+
+#endif // NFLVGL_NFLVGL_NFLVGL_LVGL_H
